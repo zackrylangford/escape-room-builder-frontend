1db11ad1ac638794ba59985902d912b8af9e0ad2
Zackry Langford <zackrylangford@gmail.com>
2023-12-05 19:22:23 -0500

    updated to working CRUD

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,121 +1,43 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetchGames();
-});
-
-function fetchGames() {
-    fetch('https://4mqvypnc0d.execute-api.us-east-1.amazonaws.com/v1/games')
+document.getElementById('getAllItems').addEventListener('click', function() {
+    fetch('https://sfw4prb6a8.execute-api.us-east-1.amazonaws.com/Prod/')
         .then(response => response.json())
         .then(data => {
-            console.log('Fetched games:', data); // Log fetched data
-            populateTable(data);
-        })
-        .catch(error => {
-            console.error('Error fetching data: ', error);
+            document.getElementById('items').innerText = JSON.stringify(data, null, 2);
         });
-}
+});
 
-function populateTable(games) {
-    const tableBody = document.getElementById('gamesTableBody');
-    tableBody.innerHTML = ''; // Clear existing table data
-
-    games.forEach(game => {
-        let row = tableBody.insertRow();
-        row.setAttribute('data-game-id', game.game_id); // Set the data-game-id attribute
-
-        let gameIdCell = row.insertCell(0);
-        gameIdCell.textContent = game.game_id;
-
-        let descriptionCell = row.insertCell(1);
-        descriptionCell.textContent = game.description || 'No description available';
-
-        let editCell = row.insertCell(2); // New cell for edit button
-        let editButton = document.createElement('button');
-        editButton.textContent = 'Edit';
-        editButton.onclick = function() { editGame(game.game_id); };
-        editCell.appendChild(editButton);
-        let deleteCell = row.insertCell(3); // New cell for delete button
-        let deleteButton = document.createElement('button');
-        deleteButton.textContent = 'Delete';
-        deleteButton.onclick = function() { deleteGame(game.game_id, row); };
-        deleteCell.appendChild(deleteButton);
-    });
-}
-
-function editGame(game_id) {
-    const row = document.querySelector(`tr[data-game-id='${game_id}']`);
-    const descriptionCell = row.cells[1];
-    const currentDescription = descriptionCell.textContent;
-
-    descriptionCell.innerHTML = `<input type="text" value="${currentDescription}">`;
-
-    const editButton = row.cells[2].querySelector('button');
-    editButton.textContent = 'Save';
-    editButton.onclick = function() { saveGame(game_id); };
-}
-
-function saveGame(gameId) {
-    const row = document.querySelector(`tr[data-game-id='${game_id}']`);
-    const newDescription = row.cells[1].querySelector('input').value;
-
-    console.log('Saving game:', { gameId, newDescription }); // Log the data being saved
-
-    sendNewDescription(game_id, newDescription)
-        .then(() => {
-            row.cells[1].textContent = newDescription;
-            const editButton = row.cells[2].querySelector('button');
-            editButton.textContent = 'Edit';
-            editButton.onclick = function() { editGame(game_id); };
-        })
-        .catch(error => {
-            console.error('Error:', error);
+document.getElementById('getItem').addEventListener('click', function() {
+    const id = document.getElementById('getItemId').value;
+    fetch(`https://sfw4prb6a8.execute-api.us-east-1.amazonaws.com/Prod/${id}`)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('singleItem').innerText = JSON.stringify(data, null, 2);
         });
-}
+});
 
-function sendNewDescription(gameId, newDescription) {
-    const url = `https://4mqvypnc0d.execute-api.us-east-1.amazonaws.com/v1/games/${game_id}`;
-    const body = JSON.stringify({
-        description: newDescription
-    });
+document.getElementById('deleteItem').addEventListener('click', function() {
+    const id = document.getElementById('deleteItemId').value;
+    console.log("Delete this id please: " + id);
+    fetch(`https://sfw4prb6a8.execute-api.us-east-1.amazonaws.com/Prod/${id}`, { method: 'DELETE' })
+        .then(response => response.json())
+        .then(data => {
+            alert('Item deleted');
+        });
+});
 
-    console.log('Sending PUT request with body:', body);
-
-    return fetch(url, {
-        method: 'PUT',  // Change to PUT for update operations
+document.getElementById('addItem').addEventListener('click', function() {
+    const id = document.getElementById('newItemId').value;
+    const name = document.getElementById('newItemName').value;
+    fetch('https://sfw4prb6a8.execute-api.us-east-1.amazonaws.com/Prod/', { 
+        method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: body
+        body: JSON.stringify({ id, name })
     })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-    })
-    .catch(error => {
-        console.error('Error updating game:', error);
-    });
-}
+        .then(response => response.json())
+        .then(data => {
+            alert('Item added');
+        });
+});
 
-function deleteGame(gameId, row) {
-const url = `https://4mqvypnc0d.execute-api.us-east-1.amazonaws.com/v1/games/${gameId}`;
-console.log('Sending DELETE request for game:', gameId);
-
-
-    fetch(url, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        console.log('Game deleted successfully:', gameId);
-        row.remove(); // Remove the game's row from the table
-    })
-    .catch(error => {
-        console.error('Error deleting game:', error);
-    });
-}
