8927a8d44a627f8679e3830cb9eef0e0e996355d
Zackry Langford <zackrylangford@gmail.com>
2023-12-07 14:13:51 -0500

    basic form created, added placeholder data

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
+
+//function to get the id token from local storage and add it to the headers of the fetch request
 function fetchWithAuth(url, options = {}) {
     const idToken = localStorage.getItem("IdToken");
     console.log("Retrieved Token: ", idToken);
@@ -28,6 +30,7 @@ function fetchWithAuth(url, options = {}) {
         });
 }
 
+// Get all the items from the DynamoDB table 
 document.getElementById('getAllItems').addEventListener('click', function() {
     fetchWithAuth('https://sfw4prb6a8.execute-api.us-east-1.amazonaws.com/Prod/')
         .then(data => {
@@ -39,6 +42,7 @@ document.getElementById('getAllItems').addEventListener('click', function() {
         });
 });
 
+// Get a single item from the DynamoDB table
 document.getElementById('getItem').addEventListener('click', function() {
     const id = document.getElementById('getItemId').value;
     fetchWithAuth(`https://sfw4prb6a8.execute-api.us-east-1.amazonaws.com/Prod/${id}`)
@@ -50,6 +54,7 @@ document.getElementById('getItem').addEventListener('click', function() {
         });
 });
 
+// Delete an item from the DynamoDB table
 document.getElementById('deleteItem').addEventListener('click', function() {
     const id = document.getElementById('deleteItemId').value;
     fetchWithAuth(`https://sfw4prb6a8.execute-api.us-east-1.amazonaws.com/Prod/${id}`, { method: 'DELETE' })
@@ -62,21 +67,51 @@ document.getElementById('deleteItem').addEventListener('click', function() {
         });
 });
 
-document.getElementById('addItem').addEventListener('click', function() {
-    const id = document.getElementById('newItemId').value;
-    const name = document.getElementById('newItemName').value;
+// Add a game to the DynamoDB table
+document.getElementById('saveGame').addEventListener('click', function() {
+    // Get the values from the form
+    const gameTitle = document.getElementById('gameTitle').value;
+    const gameDescription = document.getElementById('gameDescription').value;
+    const timeLimit = document.getElementById('timeLimit').value;
+    
+    // Get the challenge data
+    const challenges = [];
+    const challengeElements = document.querySelectorAll('.challenge');
+    challengeElements.forEach(challengeElement => {
+        const challengeTitle = challengeElement.querySelector('.challengeTitle').value;
+        const challengeDescription = challengeElement.querySelector('.challengeDescription').value;
+        const challengeType = challengeElement.querySelector('.challengeType').value;
+        
+        // Add the challenge data to the array
+        challenges.push({
+            title: challengeTitle,
+            description: challengeDescription,
+            type: challengeType
+        });
+    });
+
+    // Create a game object
+    const gameData = {
+        title: gameTitle,
+        description: gameDescription,
+        timeLimit: parseInt(timeLimit), // Convert to integer
+        challenges: challenges
+    };
+
+    // Send the game data to Lambda
     fetchWithAuth('https://sfw4prb6a8.execute-api.us-east-1.amazonaws.com/Prod/', { 
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ id, name })
+        body: JSON.stringify(gameData)
     })
-        .then(data => {
-            alert('Item added');
-            console.log("Add response: ", data);
-        })
-        .catch(error => {
-            console.error("Error adding item: ", error);
-        });
+    .then(data => {
+        alert('Game added successfully');
+        console.log("Add game response: ", data);
+    })
+    .catch(error => {
+        console.error("Error adding game: ", error);
+    });
 });
+
