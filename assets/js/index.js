5f350d88fd8ea1a13edcd87b9d0944fcfc4196db
Zackry Langford <zackrylangford@gmail.com>
2023-12-07 20:40:10 -0500

    refactor and organization of index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,34 +1,5 @@
-
-//function to get the id token from local storage and add it to the headers of the fetch request
-function fetchWithAuth(url, options = {}) {
-    const idToken = localStorage.getItem("IdToken");
-    console.log("Retrieved Token: ", idToken);
-
-    if (!idToken) {
-        console.log("No token found. Redirecting to login.");
-        window.location.href = '/login.html';
-        return;
-    }
-
-    const headers = {
-        'Authorization': 'Bearer ' + idToken,
-        ...options.headers
-    };
-
-    const fetchOptions = { ...options, headers };
-    console.log("Fetching URL with options: ", url, JSON.stringify(fetchOptions, null, 2));
-
-    return fetch(url, fetchOptions)
-        .then(response => {
-            console.log("Raw Response: ", response);
-
-            if (response.ok && response.headers.get('content-type')?.includes('application/json')) {
-                return response.json();
-            } else {
-                return response.text();
-            }
-        });
-}
+// Call to setup the challenge dropdown when the page initially loads
+populateChallengeDropdown();
 
 // Function to fetch the list of challenges from your /challenges API endpoint
 function fetchChallenges() {
@@ -58,62 +29,11 @@ function populateChallengeDropdown() {
         });
 }
 
-
-// Add event listener to "Save Game" button to save the game data to the specified API endpoint
-document.getElementById('saveGame').addEventListener('click', function() {
-    // Get the values from the form
-    const gameTitle = document.getElementById('gameTitle').value;
-    const gameDescription = document.getElementById('gameDescription').value;
-    const timeLimit = document.getElementById('timeLimit').value;
-
-    // Get the challenge data
-    const challenges = [];
-    const challengeElements = document.querySelectorAll('.challenge');
-    challengeElements.forEach(challengeElement => {
-        const challengeTitle = challengeElement.querySelector('.challengeTitle').value;
-        const challengeDescription = challengeElement.querySelector('.challengeDescription').value;
-        const challengeType = challengeElement.querySelector('.challengeType').value;
-
-        // Add the challenge data to the array
-        challenges.push({
-            title: challengeTitle,
-            description: challengeDescription,
-            type: challengeType
-        });
-    });
-
-    // Create a game object
-    const gameData = {
-        title: gameTitle,
-        description: gameDescription,
-        timeLimit: parseInt(timeLimit), // Convert to integer
-        challenges: challenges
-    };
-
-    // Send the game data to the specified API endpoint (replace '/your-api-endpoint' with the actual endpoint)
-fetchWithAuth('https://sfw4prb6a8.execute-api.us-east-1.amazonaws.com/Prod/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(gameData)
-    })
-        .then(data => {
-            alert('Game added successfully');
-            console.log("Add game response: ", data);
-        })
-        .catch(error => {
-            console.error("Error adding game: ", error);
-        });
-});
-
-
 // Add event listener to "Add Challenge" button to create a new challenge input form
 document.getElementById('addChallenge').addEventListener('click', function() {
     console.log("Adding challenge button clicked");
     createChallengeInput();
 });
-
 
 // Function to populate the challenge type select element
 function populateChallengeTypeSelect(challengeTypeSelect) {
@@ -132,7 +52,6 @@ function populateChallengeTypeSelect(challengeTypeSelect) {
 }
 
 // Function to create a new challenge input form
-
 function createChallengeInput() {
     const challengeContainer = document.getElementById('challengesContainer');
     const newChallenge = document.createElement('div');
@@ -179,6 +98,81 @@ function createChallengeInput() {
     challengeContainer.appendChild(newChallenge);
 }
 
+// Add event listener to "Save Game" button to save the game data to the specified API endpoint
+document.getElementById('saveGame').addEventListener('click', function() {
+    // Get the values from the form
+    const gameTitle = document.getElementById('gameTitle').value;
+    const gameDescription = document.getElementById('gameDescription').value;
+    const timeLimit = document.getElementById('timeLimit').value;
 
-// Populate the challenge dropdown when the page loads
-populateChallengeDropdown();
+    // Get the challenge data
+    const challenges = [];
+    const challengeElements = document.querySelectorAll('.challenge');
+    challengeElements.forEach(challengeElement => {
+        const challengeTitle = challengeElement.querySelector('.challengeTitle').value;
+        const challengeDescription = challengeElement.querySelector('.challengeDescription').value;
+        const challengeType = challengeElement.querySelector('.challengeType').value;
+
+        // Add the challenge data to the array
+        challenges.push({
+            title: challengeTitle,
+            description: challengeDescription,
+            type: challengeType
+        });
+    });
+
+    // Create a game object
+    const gameData = {
+        title: gameTitle,
+        description: gameDescription,
+        timeLimit: parseInt(timeLimit), // Convert to integer
+        challenges: challenges
+    };
+
+    // Send the game data to the specified API endpoint (replace '/your-api-endpoint' with the actual endpoint)
+    fetchWithAuth('https://sfw4prb6a8.execute-api.us-east-1.amazonaws.com/Prod/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(gameData)
+    })
+        .then(data => {
+            alert('Game added successfully');
+            console.log("Add game response: ", data);
+        })
+        .catch(error => {
+            console.error("Error adding game: ", error);
+        });
+});
+
+//function to get the id token from local storage and add it to the headers of the fetch request
+function fetchWithAuth(url, options = {}) {
+    const idToken = localStorage.getItem("IdToken");
+    console.log("Retrieved Token: ", idToken);
+
+    if (!idToken) {
+        console.log("No token found. Redirecting to login.");
+        window.location.href = '/login.html';
+        return;
+    }
+
+    const headers = {
+        'Authorization': 'Bearer ' + idToken,
+        ...options.headers
+    };
+
+    const fetchOptions = { ...options, headers };
+    console.log("Fetching URL with options: ", url, JSON.stringify(fetchOptions, null, 2));
+
+    return fetch(url, fetchOptions)
+        .then(response => {
+            console.log("Raw Response: ", response);
+
+            if (response.ok && response.headers.get('content-type')?.includes('application/json')) {
+                return response.json();
+            } else {
+                return response.text();
+            }
+        });
+}
