8cb06e5756449d4a01f3732d8b842806d97613d9
Zackry Langford <zackrylangford@gmail.com>
2023-12-12 10:41:02 -0500

    added edit icon to each escape room game card

--- assets/js/gameManager.js
+++ assets/js/gameManager.js
@@ -8,39 +8,49 @@ export function displayAllGames() {
     fetchWithAuth('https://sfw4prb6a8.execute-api.us-east-1.amazonaws.com/Prod/', {
         method: 'GET',
     })
-        .then(games => {
-            allGamesList.innerHTML = ''; // Clear existing content
+    .then(games => {
+        allGamesList.innerHTML = ''; // Clear existing content
 
-            games.forEach(game => {
-                const gameContainer = document.createElement('div');
-                gameContainer.classList.add('game');
-
-                // Add game details to the card
-                const gameTitle = document.createElement('h3');
-                gameTitle.textContent = game.GameTitle.S;
-                gameContainer.appendChild(gameTitle);
-
-                const gameDescription = document.createElement('p');
-                gameDescription.textContent = game.GameDescription.S;
-                gameContainer.appendChild(gameDescription);
-
-                const timeLimit = document.createElement('p');
-                timeLimit.textContent = `Time Limit: ${game.TimeLimit.N} seconds`;
-                gameContainer.appendChild(timeLimit);
-
-                // Add challenges list
-                const challengesList = document.createElement('ul');
-                game.Challenges.L.forEach(challenge => {
-                    const challengeItem = document.createElement('li');
-                    challengeItem.textContent = `${challenge.M.Title.S}: ${challenge.M.Description.S} (Type: ${challenge.M.Type.S})`;
-                    challengesList.appendChild(challengeItem);
-                });
-                gameContainer.appendChild(challengesList);
-
-                allGamesList.appendChild(gameContainer);
+        games.forEach(game => {
+            const gameContainer = document.createElement('div');
+            gameContainer.classList.add('game');
+        
+            // Create and append the edit icon to the game container
+            const editIcon = document.createElement('span');
+            editIcon.classList.add('edit-icon');
+            editIcon.innerHTML = '&#9998;'; // Unicode for a pencil icon
+            editIcon.addEventListener('click', function() {
+                // TODO: Handle edit click
+                console.log('Edit icon clicked for game:', game.GameTitle.S);
             });
-        })
-        .catch(error => {
-            console.error("Error fetching games: ", error);
+            gameContainer.appendChild(editIcon);
+
+            // Add game details to the card
+            const gameTitle = document.createElement('h3');
+            gameTitle.textContent = game.GameTitle.S;
+            gameContainer.appendChild(gameTitle);
+
+            const gameDescription = document.createElement('p');
+            gameDescription.textContent = game.GameDescription.S;
+            gameContainer.appendChild(gameDescription);
+
+            const timeLimit = document.createElement('p');
+            timeLimit.textContent = `Time Limit: ${game.TimeLimit.N} seconds`;
+            gameContainer.appendChild(timeLimit);
+
+            // Add challenges list
+            const challengesList = document.createElement('ul');
+            game.Challenges.L.forEach(challenge => {
+                const challengeItem = document.createElement('li');
+                challengeItem.textContent = `${challenge.M.Title.S}: ${challenge.M.Description.S} (Type: ${challenge.M.Type.S})`;
+                challengesList.appendChild(challengeItem);
+            });
+            gameContainer.appendChild(challengesList);
+
+            allGamesList.appendChild(gameContainer);
         });
+    })
+    .catch(error => {
+        console.error("Error fetching games: ", error);
+    });
 }
